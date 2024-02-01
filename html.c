1997deca781d393959fd1271b8f8910df7185a08
Tanisha-5 <156989009+Tanisha-5@users.noreply.github.com>
2024-02-01 10:58:38 +0530

    Add files via upload

---
+++ html.c
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <style>
+   body{background-color:red;}
+  </style>
+ </head>
+    <body>
+     <h1> Borderline Braindead! </h1>
+    <p> This is a dialogue </p>
+    </body>
+</html>
